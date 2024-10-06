b994993d35371456aae98761bba0a93898751de5
gideonadeti <gideonadeti0@gmail.com>
2024-10-06 03:48:36 +0000

    Add handleKeyClick prop

--- src/components/Key.tsx
+++ src/components/Key.tsx
@@ -1,13 +1,17 @@
 import { KeyProps } from "../types";
 import { operators } from "../lib/calculator-data";
 
-export default function Key({ keyData: { id, value } }: KeyProps) {
+export default function Key({
+  keyData: { id, value },
+  handleKeyClick,
+}: KeyProps) {
   const isOperator = operators.includes(value as string);
 
   return (
     <button
       className={`btn fs-5 border rounded ${isOperator ? "text-primary" : ""}`}
       id={id}
+      onClick={() => handleKeyClick(value as string)}
     >
       {value}
     </button>
--- src/components/Keys.tsx
+++ src/components/Keys.tsx
@@ -1,11 +1,15 @@
 import { KeysProps } from "../types";
 import Key from "./Key";
 
-export default function Keys({ keysData }: KeysProps) {
+export default function Keys({ keysData, handleKeyClick }: KeysProps) {
   return (
     <div className="keys">
       {keysData.map((keyData) => (
-        <Key key={keyData.id} keyData={keyData} />
+        <Key
+          key={keyData.id}
+          keyData={keyData}
+          handleKeyClick={handleKeyClick}
+        />
       ))}
     </div>
   );
