8a412bf863523d81d21c026aa7d3683065f683b5
gideonadeti <gideonadeti0@gmail.com>
2024-10-06 04:11:25 +0000

    Extract operators from special keys

--- src/lib/calculator-data.ts
+++ src/lib/calculator-data.ts
@@ -20,6 +20,8 @@ export const keys = [
   { id: "decimal", value: "." },
 ];
 
-export const operators = ["C", "÷", "x", "⌫", "-", "+", "="];
+export const operators = ["÷", "x", "-"];
+
+export const specialKeys = ["C", "÷", "x", "⌫", "-", "+", "="];
 
 export const numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
