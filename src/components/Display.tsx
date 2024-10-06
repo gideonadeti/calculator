7d9179c28cf7840f60b98d2945d1630f16335c26
gideonadeti <gideonadeti0@gmail.com>
2024-10-06 03:48:08 +0000

    Restyle display

--- src/components/Display.tsx
+++ src/components/Display.tsx
@@ -3,10 +3,10 @@ import { DisplayProps } from "../types";
 export default function Display({ input, output }: DisplayProps) {
   return (
     <div
-      className="rounded border p-3"
+      className="border rounded p-1 fs-4 text-end d-flex flex-column justify-content-between bg-light"
       style={{ width: "100%", height: "12vh" }}
     >
-      <span>{output}</span>
+      <span style={{ minHeight: "5vh" }}>{output}</span>
       <span>{input}</span>
     </div>
   );
