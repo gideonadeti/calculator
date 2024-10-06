4d47ab1e76217373b9edd72e39cb2ffa92ea93dd
gideonadeti <gideonadeti0@gmail.com>
2024-10-06 04:09:36 +0000

    Set overflow to hidden

--- src/components/Display.tsx
+++ src/components/Display.tsx
@@ -3,7 +3,7 @@ import { DisplayProps } from "../types";
 export default function Display({ input, output }: DisplayProps) {
   return (
     <div
-      className="border rounded p-1 fs-4 text-end d-flex flex-column justify-content-between bg-light"
+      className="border rounded p-1 fs-4 text-end d-flex flex-column justify-content-between bg-light overflow-hidden"
       style={{ width: "100%", height: "12vh" }}
     >
       <span style={{ minHeight: "5vh" }}>{output}</span>
